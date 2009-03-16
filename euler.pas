cb9e07ace35a18e3bd0604177707ce1689c7647c
John Evans <lgastako@gmail.com>
2009-03-15 23:24:19 -0700

    euler1 done in pascal

--- euler.pas
+++ euler.pas
@@ -1,4 +1,35 @@
+(* Project Euler in pascal (specifically targeting the GNU pascal compiler) 
+
+To compile:
+% gpc euler.pas -o euler
+
+*)
+
 program euler;
 
+    (* Euler #1
+       Answer: 233168
+
+       If we list all the natural numbers below 10 that are multiples of 3 or 5,
+       we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+       Find the sum of all the multiples of 3 or 5 below 1000.
+    *)
+    function euler1 : integer;
+    var n : integer;
+    var i : integer;
+    begin
+        n := 0;
+        for i := 1 to 999 do
+        begin
+            if (i mod 3 = 0) or (i mod 5 = 0) then
+            begin
+                n := n + i;
+            end;
+        end;
+        euler1 := n;
+    end;
+
 begin
+    writeln(euler1());
 end.
