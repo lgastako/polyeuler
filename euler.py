28dcc937c3694454b48b030cd5ad426a686a9f49
John Evans <lgastako@gmail.com>
2009-03-17 20:14:37 -0700

    Added verified solution for problem #4 in python.

--- euler.py
+++ euler.py
@@ -69,10 +69,34 @@ def euler3():
         n -= 1
 
 
+def euler4():
+    """Problem #4
+    Answer: 906609
+
+    A palindromic number reads the same both ways. The largest
+    palindrome made from the product of two 2-digit numbers is 9009 =
+    91 99.
+
+    Find the largest palindrome made from the product of two 3-digit
+    numbers.
+
+    """
+
+    r = 0
+    for a in range(100, 1000):
+        for b in range(100, 1000):
+            c = a * b
+            s = "%d" % c
+            if s == s[::-1]:
+                r = max(c, r)
+    return r
+
+
 EULERS = [
     euler1,
     euler2,
-    euler3
+    euler3,
+    euler4
 ]
 
 
