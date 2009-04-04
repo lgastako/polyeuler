b4dda99d539bfecb0a978d965f4391ab8549764a
John Evans <lgastako@gmail.com>
2009-04-04 00:18:05 -0700

    Added #6 in perl.

--- euler.pl
+++ euler.pl
@@ -166,6 +166,31 @@ sub euler5
 }
 
 
+# Problem #6
+# Answer: 25164150
+#
+# The sum of the squares of the first ten natural numbers is,
+#     1² + 2² + ... + 10² = 385
+# The square of the sum of the first ten natural numbers is,
+#     (1 + 2 + ... + 10)² = 55² = 3025
+# Hence the difference between the sum of the squares of the first
+# ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+#
+# Find the difference between the sum of the squares of the first one
+# hundred natural numbers and the square of the sum.
+
+sub euler6
+{
+    my ($i, $sum, $sum_squares);
+    $sum = 0;
+    $sum_squares = 0;
+    for ($i=1; $i<=100; $i++) {
+        $sum += $i;
+        $sum_squares += ($i * $i);
+    }
+    return ($sum * $sum) - $sum_squares;
+}
+
 
 sub main {
     my @EULERS = (
