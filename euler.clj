ee82b2ade52d6f402402721d417310a1da6cb30d
John Evans <lgastako@gmail.com>
2009-05-31 16:01:55 -0700

    Added #12 in Clojure.

--- euler.clj
+++ euler.clj
@@ -350,4 +350,23 @@
 ;;(defn euler11 []
 ;;  (find-lowest-triangle-number-with-more-than-n-divisors 500))
 
+;; Problem 12
+;; Answer: 1366
+;; 2^(15) = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+;; What is the sum of the digits of the number 2^(1000)?
+
+(defn expt
+  ([n p] (expt n n p))
+  ([r n p]
+    (if (<= p 1)
+      r
+      (recur (* n r) n (dec p)))))
+
+(defn euler12 []
+  (apply +
+   (map #(Character/digit % 10)
+        (str (expt 2 1000)))))
+
 ;; TODO: use with-command-line-args to do some stuff.
+
+
